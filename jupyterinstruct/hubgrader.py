eff203ba467dfa1eada5114a1184e6528b768550
Dirk Colbry <colbrydi@msu.edu>
2021-01-27 17:50:39 -0500

    adding in automated feedback email

--- jupyterinstruct/hubgrader.py
+++ jupyterinstruct/hubgrader.py
@@ -9,7 +9,6 @@ output = hubgrader.importnb(studentfile)
 """
 
 
-
 from jupyterinstruct.nbfilename import nbfilename
 from pathlib import Path
 from IPython.core.display import Javascript, HTML
@@ -18,6 +17,21 @@ import shutil
 import time
 import pathlib
 from pathlib import Path
+
+def makestudents(filename="students.csv"):
+    """Generate student accounts in nbgrader from a CSV file exported from D2L"""
+    import pandas as pd
+    import subprocess
+
+    namefile = pd.read_csv(filename)
+    for index, row in namefile.iterrows():
+        first = row['First Name']
+        last = row['Last Name']
+        email = f"{row['Email']}@msu.edu"
+        name = f"{first}_{last}"
+        command = f"nbgrader db student add {name} --last-name={last} --first-name={first} --email={email}"
+        print(command)
+        returned_output = subprocess.check_output(command, shell=True)
 
 class gradernames():
     """Create a class of names that follow the nbgrader naming convention:
@@ -137,24 +151,31 @@ def quick_review_D2L(zipfile="nbTester_data.zip", folder='unziptemp'):
         markdown += f"- [{htmlfile}]({htmlfile})\n"
     return markdown
 
+def D2L_2_nbgrader(zipfile, assignment, destination='AutoGrader/submitted', tempfolder='upziptemp'):
+    unpackD2L(zipfile, destination=tempfolder)
+    expandfiles(assignment, source=tempfolder, destination=destination)
+          
 def expandfiles(assignment, source='upziptemp', destination='AutoGrader/submitted'):
+    nbfile = nbfilename(assignment)
+    nbfile.isInstructor = False
+    nbfile.isStudent = False
     sourcefolder = Path(source)
     ipynbfiles = sourcefolder.glob('*.ipynb')
     for thisfile in ipynbfiles:
         print(thisfile.stem)
-        myfolder = Path(destination / Path(thisfile.stem))
+        myfolder = Path(destination / Path(thisfile.stem) / Path(nbfile.basename()))
         myfolder.mkdir(parents=True, exist_ok=True)
-        thisfile.rename(myfolder / Path(assignment))
-
+        nbfile.isStudent = True
+        thisfile.rename(myfolder / Path(assignment + nbfile.basename()) )
     
 
 def unpackD2L(filename, destination='upziptemp'):
     import warnings
-    print("unpackD2L will be deprecated in the future and moved to a different package (See documentation for updates)")
-    warnings.warn(
-        "unpackD2L will be deprecated in the future and moved to a different package (See documentation for updates)",
-        DeprecationWarning
-    )
+#     print("unpackD2L will be deprecated in the future and moved to a different package (See documentation for updates)")
+#     warnings.warn(
+#         "unpackD2L will be deprecated in the future and moved to a different package (See documentation for updates)",
+#         DeprecationWarning
+#     )
  
     #from urllib.request import urlretrieve
     import zipfile
@@ -175,4 +196,116 @@ def unpackD2L(filename, destination='upziptemp'):
         #print(destination_folder / newfile)
         f.rename(Path(destination / newfile))
 
+          
+def sendfeedback(assignment_name, 
+                 subject='MTH314 Assignment Feedback', 
+                 graded_message=None,
+                 empty_feedback=None,
+                 cc='',
+                 bcc='',
+                 feedback_folder = f"./AutoGrader/feedback/",
+                 EMAILtest=True):
+    
+    import getpass
+    import smtplib
+    import subprocess
+    import smtplib
+    from email.mime.text import MIMEText
+    from pathlib import Path
+    from email.mime.application import MIMEApplication
+    from email.mime.multipart import MIMEMultipart
+    from os.path import basename
 
+          
+    if(EMAILtest):
+        print("TESTING Only, Emails will NOT be sent")
+    else:
+        for i in range(0,3):
+            print("WARNING, DANGER!!!!, Emails are being Sent")
+          
+    #Get the current username for the "from" email
+    usr=getpass.getuser()
+    fromAddress=usr+"@msu.edu"
+    print(f"My email is {fromAddress}\n\n")
+                 
+    #Pull in student names and emails from the nbgrader database
+    command = "nbgrader db student list > temp.names"
+    returned_output = subprocess.check_output(command, shell=True)
+    s = smtplib.SMTP('smtp.egr.msu.edu')
+    getdb = open("temp.names", 'r')
+    lines = getdb.readlines()
+    
+    emails = []
+    tags =dict()
+    tags['assignment_name']=assignment_name
+    for line in lines:
+        part = line.split(' ')
+        if not part[0] == 'There':
+            folder = part[0]
+            tags['last'] = part[1][1:-1]
+            tags['first'] = part[2][:-1]
+
+            toAddress = part[4][:-1]
+            pth = Path(f"{feedback_folder}{folder}/{assignment_name}/")
+            files = list(pth.glob('*.html'))
+
+            if files:
+                message=graded_message
+            else:
+                message=empty_feedback
+            
+            if not message == None:
+                print(f"Emailing {tags['last']} {tags['first']}")
+
+                newmessage=message
+
+                for j,tag in enumerate(tags):
+                    newmessage = newmessage.replace(f"<<<{tag}>>>", tags[tag])
+
+                msg = MIMEMultipart()
+                msg.attach(MIMEText(newmessage))
+                msg['Subject'] = subject
+                msg['From'] = fromAddress
+                msg['To'] = toAddress
+
+                #Attach feedback
+                if files:
+                    feedback_file = files[0]        
+                    print(f"Attaching file {feedback_file}")
+                    with open(feedback_file, "rb") as attachment_file:
+                        part = MIMEApplication(attachment_file.read(),Name=basename(feedback_file))
+                    # After the file is closed
+                    part['Content-Disposition'] = f'attachment; filename="{basename(feedback_file)}"'
+                    msg.attach(part)
+                else:
+                    feedback_file="NONE"
+
+                rcpt = toAddress.split(",")
+                if cc:
+                    rcpt = cc.split(",") + rcpt
+                    msg['Cc'] = cc
+                if bcc:
+                    rcpt = bcc.split(",") + rcpt
+                    msg['Bcc'] = bcc
+
+                #TODO Add code to verify all << have been found
+                #print(rcpt)
+                if EMAILtest:
+                    print('To:',toAddress)
+                    print('From:',fromAddress)
+                    if cc:
+                        print('CC:',cc)
+                    if bcc:
+                        print('BCC:', bcc)
+                    print('Subject:', subject)
+                    print('Attachment:', feedback_file)
+                    print(newmessage)
+                else:
+                    try:
+                        # Send the message via engineering SMTP server.
+                        print('Connecting to SMTP and sending message to', toAddress)
+                        s.sendmail(fromAddress, rcpt, msg.as_string())
+                    except SMTPException:
+                        print("Error: unable to send email to", toAddress)
+                print("\n\n")
+    s.quit()
